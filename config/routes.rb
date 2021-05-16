8493e609185a301fe5774fc35104a78f0c132740
Mira Chami <miramc149@gmail.com>
2021-05-16 20:12:49 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   get '/birds' => 'birds#index'
+  get '/birds/:id' => 'birds#show' # new
+end
+class BirdsController < ApplicationController
+  def index
+    birds = Bird.all
+    render json: birds
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
+  end
+end
+
+def show
+  bird = Bird.find_by(id: params[:id])
+  render json: {id: bird.id, name: bird.name, species: bird.species }
 end
